0e0207d96ae465f8d5e9c6ff5bb1ee3ab75bfb74
OhDaky <ttakko88@gmail.com>
2021-06-23 01:24:42 +0900

    Nav underbar & hover color

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -11,16 +11,16 @@ function NavBar() {
           <span className="logo_white"></span>
           <span className="home">COAWAY</span>
         </Link>
-        <Link to='/local'>
+        <Link to='/local' style={{textDecoration: 'none'}}>
           <span className="menu">지역별현황</span>
         </Link>
-        <Link to='/inoculation'>
+        <Link to='/inoculation' style={{textDecoration: 'none'}}>
           <span className="menu">접종현황</span>
         </Link>
-        <Link to='/precautions'>
+        <Link to='/precautions' style={{textDecoration: 'none'}}>
           <span className="menu">예방수칙</span>
         </Link>
-        <Link to='/vaccineInfo'>
+        <Link to='/vaccineInfo' style={{textDecoration: 'none'}}>
           <span className="menu">백신정보</span>
         </Link>
       </span>
