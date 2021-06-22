e875ce5d28ecda5eed08df4b74f65ac26c257ace
OhDaky <ttakko88@gmail.com>
2021-06-23 01:06:48 +0900

    Nav modified

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,9 +7,21 @@ function NavBar() {
   return (
     <div>
       <span className='header_original'>
-        <Link to='/home'>
+        <Link to='/'>
           <span className="logo_white"></span>
-          <span className="header">COAWAY</span>
+          <span className="home">COAWAY</span>
+        </Link>
+        <Link to='/local'>
+          <span className="menu">지역별현황</span>
+        </Link>
+        <Link to='/inoculation'>
+          <span className="menu">접종현황</span>
+        </Link>
+        <Link to='/precautions'>
+          <span className="menu">예방수칙</span>
+        </Link>
+        <Link to='/vaccineInfo'>
+          <span className="menu">백신정보</span>
         </Link>
       </span>
     </div>
